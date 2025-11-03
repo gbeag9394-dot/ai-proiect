559ed97f74bf0a3f89cccd8b8f4597096eceb0eb
gbeag9394-dot <gbeag9394@gmail.com>
2025-11-03 13:20:36 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import streamlit as st
 st.title('나의 첫 웹 서비스 만들기')
-name=st.text_input('이름을 입력하세요:')
+현아=st.text_input('이름을 입력하세요:')
 if st.button('인사말 생성'):
-  st.write(name+'님! 안녕하세요')
+  st.write(현+'님! 안녕하세요')
   st.balloons()
+st.warning('방가')
+st.error('정말 반가워요')
+st.balloons()
